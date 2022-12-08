ce03312e6d51e22a7394aabeb6fe38cf1ed3121c
Your Name <shaxboz.yaxyoyev@bk.ru>
2022-12-08 11:15:03 +0500

    skills done

--- src/components/Home/Skills/index.jsx
+++ src/components/Home/Skills/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Container, HeaderWrap, ContentWrap, Left, Right} from './style';
+import {Container, HeaderWrap, ContentWrap, Left, Right, Header, Line, Text} from './style';
+import dots from '../../../assets/images/dots.png';
+import logo from '../../../assets/images/logo.png';
 
 const Skills = () => {
   return (
@@ -10,9 +12,46 @@ const Skills = () => {
         </HeaderWrap>
         <ContentWrap>
             <Left>
-               
+               <Left.Dots1 src={dots} />
+               <Left.Box/>
+               <Left.Logo src={logo}/>
+               <Left.Dots2 src={dots}/>
+               <Left.SmallBox/>
             </Left>
-            <Right>Hey</Right>
+            <Right>
+              <Right.Container>
+                 <Right.Box1>
+                   <Header>Languages</Header>
+                   <Line/>
+                   <Text>JavaScript TypeScript</Text>
+                 </Right.Box1>
+                 <Right.Wrapper>
+                 <Right.Box1>
+                   <Header>Libraries</Header>
+                   <Line/>
+                   <Text>Material UI Styled-Components</Text>
+                 </Right.Box1>
+                 <Right.Box1>
+                   <Header>Others</Header>
+                   <Line/>
+                   <Text>Html Css SCss REST Api</Text>
+                 </Right.Box1>
+                 </Right.Wrapper>
+                 <Right.Wrapper>
+                  <Right.Box1>
+                    <Header>Tools</Header>
+                    <Line/>
+                    <Text>VSCode<br/> Font-Awesome Git Figma Mobile Simulator</Text>
+                  </Right.Box1>
+                  <Right.Box1>
+                    <Header>Frameworks</Header>
+                    <Line/>
+                    <Text>React Js <br/> Vue Js</Text>
+                  </Right.Box1>
+                 </Right.Wrapper>
+              </Right.Container>
+
+            </Right>
         </ContentWrap>
     </Container>
   )
--- src/components/Home/Skills/style.js
+++ src/components/Home/Skills/style.js
@@ -4,6 +4,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   color: #fff;
+  margin-bottom: 100px;
 `;
 
 const HeaderWrap = styled.div`
@@ -34,14 +35,96 @@ const ContentWrap = styled.div`
 `
 
 const Left = styled.div`
-  
+  flex: 1;
+  position: relative;
+`;
+
+Left.Dots1 = styled.img`
+  width: 63px;
+  height: 63px;
+  margin: 50px 0px 0px 10px;
+`;
+
+Left.Dots2 = styled.img`
+  width: 63px;
+  height: 63px;
+  position: absolute;
+  top: 160px;
+  left: 180px;
 `;
 
 
+Left.Box = styled.div`
+  width: 86px;
+  height: 86px;
+  border: 1px solid #ABB2BF;
+  position: absolute;
+  right: 150px;
+  top: 10px;
+`;
+
+Left.SmallBox = styled.div`
+  width: 52px;
+  height: 52px;
+  border: 1px solid #ABB2BF;
+  position: absolute;
+  right: 110px;
+  top: 200px;
+`;
+
+Left.Logo = styled.img`
+  width: 113px;
+  height: 113px;
+  position: absolute;
+  top: 180px;
+  left: 20px;
+`;
+
 const Right = styled.div`
-  
-`
+  flex: 1;
+  margin-top: 30px;
+`;
 
+Right.Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+Right.Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
+Right.Box1 = styled.div`
+  width: 160px;
+  height: fit-content;
+  border: 1px solid #ABB2BF;
+`;
 
-export {Container, HeaderWrap, Left, Right, ContentWrap};
+const Header = styled.div`
+  font-family: 'Fira Code';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 21px;
+  color: #FFFFFF;
+  margin: 5px;
+  width: 100%;
+`;
+
+const Line = styled.div`
+  border: 1px solid #ABB2BF;
+`;
+
+const Text = styled.div`
+  font-family: 'Fira Code';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  color: #ABB2BF;
+  padding: 10px;
+`;
+
+export {Container, HeaderWrap, Left, Right, ContentWrap, Header, Line, Text };
